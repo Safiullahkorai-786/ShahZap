f5c5f786156454c4f6be54fbad8ad83d3ea89a63
Safiullah Korai <shahzaibth3gr8@gmail.com>
2026-08-19 03:19:43 +0500

    feat: register PWA service worker

---
+++ src/components/PwaRegister.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export function PwaRegister() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator && window.location.protocol === 'https:') navigator.serviceWorker.register('/sw.js').catch(() => {})
+  }, [])
+  return null
+}
